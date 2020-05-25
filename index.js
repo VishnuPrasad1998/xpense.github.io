ca3bed3be475e3a1c8bb31abf6e2e1e02657a587
VishnuPrasad1998 <vishnu1998prasad@gmail.com>
2020-05-25 15:06:03 +0530

    UI fixes

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function addExpense(){
 
   expenseItem.desc = td;
   expenseItem.amount = tm;
-
+  expenseItem.moment = new Date();
   allExpense.push(expenseItem)
 
 
@@ -28,10 +28,7 @@ function addExpense(){
   totalExpense+=exp
   hea.textContent = `Total: ${totalExpense} `;
 
-  const allExpensehtml = allExpense.map(expense => {
-    return `
-    <li class="list-group-item list-group-item-action list-group-item-dark"><b>₹${expense.amount}</b> for <b>${expense.desc}</b></li>`
-  });
+  const allExpensehtml = allExpense.map(expense => createListItem(expense));
 
 const joinedall = allExpensehtml.join("");
 
@@ -41,3 +38,26 @@ expenseTable.innerHTML = joinedall;
 const element = document.querySelector("#btnSelector");
 
 element.addEventListener("click", addExpense, false);
+
+function getDateSring(moment)
+{
+  return moment.toLocaleDateString('en-US', { year:'numeric',month:'long' ,day:'numeric'})
+}
+
+function createListItem({ desc, amount, moment }){
+  return `
+  <li class="list-group-item list-group-item-dark list-group-item-action d-flex justify-content-between">
+            <div class="d-flex flex-column">
+              <b>${desc}</b>
+              <small>${getDateSring(moment)}</small>
+            </div>
+            <div>
+              <span class="px-5">
+                <b>₹${amount}</b>
+              </span>
+              <button type="button" class="btn btn-outline-danger btn-sm">
+                <i class="fa fa-trash" style="font-size:20px;color:black;"></i>
+              </button>
+            </div>
+          </li>`
+}
